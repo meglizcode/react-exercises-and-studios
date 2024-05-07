b60585c77288dc01c284261b8ebcb495cad148d9
meglizcode <meglizjudd@gmail.com>
2024-05-07 15:19:03 -0500

    part3 comeplete

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+
+   const [boardName,setBoardName] = useState('no boards yet!')
+
+   const boards = [
+      {label:'Bad Recipes',value:'badRecipes'},
+      {label:'Scary Mad Women',value:'scaryMadWomen'},
+      {label:'Crack and Cheese',value:'crackAndCheese'},
+   ];
 
    const handleChange = (event) => {
    }
@@ -10,6 +17,11 @@ export default function BoardAssignment () {
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+         {boards.map((board) => {
+            return (
+               <option key={board.label} value={board.value}>{board.label}</option>
+            )
+         })}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/StatusChange.jsx
+++ part3/studio/src/components/StatusChange.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react';
 
 export default function StatusChange () {
 
+   const [notes,setNotes] = useState('');
+   const [recipeStatus, setRecipesStatus] = useState
+   (false);
+
    const handleChange = (event) => {
+      setNotes(event.target.value)
    }
 
    const handleSubmit = (event) => {
+      event.preventDefault();
+      setRecipesStatus(true)
    }
 
    return (
@@ -13,12 +20,10 @@ export default function StatusChange () {
          <form onSubmit={handleSubmit}>
             <label>Have you tried this recipe? Add your notes here: <input type="text" value={notes} onChange={handleChange} />
             </label>
-            <input type="submit" />
+            <input type="submit"/>
          </form>
-
-         <p>My Recipe Notes aren't here!</p>
-
-         <p>I have not tried this recipe!</p>
+         <p>{notes}</p>
+         {recipeStatus ? <p>I have tried this recipe</p> : <p>I have not tried this recipe!</p>}
       </div>
    );
 }
